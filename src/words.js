0bae5ba8e46a53a2cf29f026c8013eb2ed475c1e
Cole Phillips <nphillips78@gmail.com>
2018-12-04 13:50:15 -0500

    created Q1 word arrays

---
+++ src/words.js
@@ -0,0 +1,29 @@
+const list1 = [
+  'me', 'orange', 'and', 'green', 'in', 'his', 'have', 'that'
+]
+
+const list2 = [
+  'go', 'white', 'zero', 'the', 'we', 'see', 'what', 'as'
+]
+
+const list3 = [
+  'she', 'of', 'be', 'my', 'two', 'a', 'ten', 'he'
+]
+
+const list4 = [ 
+  'five', 'it', 'blue', 'is', 'you', 'one', 'red', 'I'
+]
+
+const list5 = [
+  'yellow', 'for', 'black', 'no', 'seven', 'on', 'to', 'cut'
+]
+
+const list6 = [
+  'had', 'look', 'four', 'at', 'six', 'this', 'pink', 'from', 'all', 'brown'
+]
+
+const list7 = [
+  'or', 'was', 'purple', 'nine', 'my', 'jump', 'went', 'three', 'they', 'but'
+]
+
+export default { list1 } { list2 }
